f89284933a5c3fea063715146358671c18416ba3
cl.rodrigo.bonilla <rbonilla@clavisco.com>
2024-05-16 11:59:19 -0600

    filtrado

--- src/app/Pages/inicio/inicio.component.ts
+++ src/app/Pages/inicio/inicio.component.ts
@@ -1,32 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Socio } from '../../Models/Socio';
 import { SocioService } from '../../Services/socio.service';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MatCardModule, MatTableModule, MatIconModule, MatButtonModule],
+  imports: [MatCardModule, MatTableModule, MatIconModule, MatButtonModule, MatFormFieldModule, CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css',
 })
-
 
 export class InicioComponent {
   private socioServicio = inject(SocioService);
   public listaSocios: Socio[] = [];
   public displayedColumns: string[] = ['CardCode', 'CardName', 'Cedula', 'FederalTaxID', 'accion'];
+  public mostrarLista: boolean = false;
 
   obtenerSocios() {
     this.socioServicio.lista().subscribe({
-      next: (data: any) => {
-        
-        this.listaSocios = data.Data;      
+      next: (data: any) => {      
+        this.listaSocios = data.Data;
+
+        this.mostrarLista = false;
       },
       error: (err) => {
         console.log(err.message);
@@ -37,8 +40,32 @@ export class InicioComponent {
   constructor(private router: Router) {
     this.obtenerSocios();
   }
-
+  
   editar(objeto:Socio) {
     this.router.navigate(['/socio', objeto.CardCode])
+  }
+  
+  onBuscarSocio(event: any) {
+    const termino = event.target.value;
+    this.buscarSocio(termino);
+  }
+
+  buscarSocio(termino: string) {
+    if (!termino.trim()) {
+      this.obtenerSocios();
+      return;
+    }
+
+    this.socioServicio.lista().subscribe({
+      next: (data: any) => {
+        this.listaSocios = data.Data.filter((soci:Socio) =>
+          soci.CardCode.includes(termino) || soci.CardName.toLowerCase().includes(termino.toLowerCase())
+        );
+        this.mostrarLista = true;
+      },
+      error: (err) => {
+        console.log(err.message);
+      }
+    });
   }
 }
--- src/app/Services/socio.service.ts
+++ src/app/Services/socio.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { appsettings } from '../Settings/appsettings';
 import { Socio } from '../Models/Socio';
-import { ResponseAPI } from '../Models/ResponseAPI';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +9,16 @@ import { ResponseAPI } from '../Models/ResponseAPI';
 export class SocioService {
 
   private apiUrl:string = `${appsettings.apiUrl}ViewPartner`;
-  private headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjQwNTUxNTdhLWFmZDctNDBhZS04NmVhLTExMThiMDk2NmVhMiIsInN1YiI6InN5c0BjbGF2aXNjby5jb20iLCJlbWFpbCI6InN5c0BjbGF2aXNjby5jb20iLCJqdGkiOiI3MmQ4MTQ0ZC1mYTM4LTQzY2EtYmViMS1lNDBmYWRmMzYwNDAiLCJuYmYiOjE3MTU4Njc4NTAsImV4cCI6MTcxNTg3MzI1MCwiaWF0IjoxNzE1ODY3ODUwLCJpc3MiOiJodHRwczovL2NsYXZpc2NvLmNvbS8iLCJhdWQiOiJodHRwczovL2NsYXZpc2NvLmNvbS8ifQ.5Tbja_goz9JQnXIQ_iie-BGlaWUJtf_1O_wdIOSEyRyWaSdQ8dZRjAZBe3n4oKGuIke6lejWUA3U_TkbYGmeIw')
+  private headers = new HttpHeaders().set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY3YWYwY2QwLTdmOWUtNGQ1NS1iZTBjLTFkZDBlOGU2NGYzNCIsInN1YiI6InN5c0BjbGF2aXNjby5jb20iLCJlbWFpbCI6InN5c0BjbGF2aXNjby5jb20iLCJqdGkiOiIyNDQ3MTY5NS02YWQ4LTRhNGUtYmMxMC1jODI4ZjM1YTkyNWQiLCJuYmYiOjE3MTU4Nzg1NzYsImV4cCI6MTcxNTg4Mzk3NiwiaWF0IjoxNzE1ODc4NTc2LCJpc3MiOiJodHRwczovL2NsYXZpc2NvLmNvbS8iLCJhdWQiOiJodHRwczovL2NsYXZpc2NvLmNvbS8ifQ.q5G5IjSiY7No-DYBoTDJ2IQmm7jquwenIFvb7d2IKUObTfeEZjZGd1oWZNZKRHfl4snIKZ81o1cYmqnMEonxIg')
 
   constructor(private http: HttpClient) { }
 
   lista() {
     return this.http.get<Socio[]>(this.apiUrl, { headers: this.headers});
+  }
+
+  buscarPorCardCode(cardCode: string) {
+    return this.http.get<Socio[]>(`${this.apiUrl}?CardCode=${cardCode}`, { headers: this.headers});
   }
 
   obtener(CardCode:string) {
